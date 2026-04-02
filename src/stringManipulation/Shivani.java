2702023851fdc486a8f466b4cde68518a3731fd4
anubhavrajput70 <anubhavrajput193@gmail.com>
2026-04-02 17:16:46 +0530

    shivnai code

---
+++ src/stringManipulation/Shivani.java
@@ -0,0 +1,18 @@
+package stringManipulation;
+
+public class Shivani {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		i=0;i<str.length();i++)
+	{
+		if(Character.isLetter(str.charAt(i)))
+		{
+			st.append(str.charAt(i));
+		}
+	}
+	System.out.println(st.toString());
+}
+	}
+
+}
